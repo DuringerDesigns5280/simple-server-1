ddea70bb1a6b8abf7f56d8b1b5c12593d077f246
Taylor <tayjed2010@gmail.com>
2018-06-20 17:54:00 -0600

    initial commit

---
+++ data/cohorts.js
@@ -0,0 +1,25 @@
+module.exports = [{
+      "Id": "1",
+      "cohortName": "17-01-WD-DP",
+      "cohortCode": "g100",
+      "numberOfStduents": "28"
+    },
+    {
+      "Id": "2",
+      "cohortName": "17-01-DS-GT",
+      "cohortCode": "g105",
+      "numberOfStduents": "24"
+    },
+    {
+      "Id": "3",
+      "cohortName": "17-02-WD-PX",
+      "cohortCode": "g109",
+      "numberOfStduents": "30"
+    },
+    {
+      "Id": "4",
+      "cohortName": "17-03-WD-BD",
+      "cohortCode": "g110",
+      "numberOfStduents": "29"
+    }
+  ]
